3068e9a9b41399c13c31c0740643e9d83eec9d6f
Ma1hT3ach3r <posudar@gmail.com>
2017-09-13 09:11:42 -0400

    Uploading the basic files
    Uploading the basic files you need to get a p5 sketch going.

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(400,400);
+}
+
+function draw() {
+    background(50.100);
+    fill(10,100,200);
+    ellipse(mouseX,mouseY,50,50);
+    console.log("Check,Check");
+}
